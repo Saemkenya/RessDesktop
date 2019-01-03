fe870e8423b1d7423d9ada3d11d90ef59180922f
Saem Kenya <187comps@gmail.com>
2019-01-03 18:19:35 +0300

    User SignUp Minimal user sign-up form

--- views/createUser.jsx
+++ views/createUser.jsx
@@ -3,47 +3,95 @@ import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
 class CraeteUser extends Component {
-
-    state = {
-        salute: '',
-        name: ''
+    constructor () {
+        super()
+        this.state = {
+        salute : '',
+        name : '',
+        email : '',
+        password : '',
+        }
+        this._createUser = this._createUser.bind(this);
+        
     }
 
     render() {
         return(
             <div>
-                <div className="flex flex-column mt3">
-                    <input
-                        value={ this.state.salute } 
-                        type="text" 
-                        className="mb2"
-                        onChange={ (e) => this.setState({
-                            salute: e.target.value
-                        }) }
-                        placeholder='New user salute'/>
+                <article className="pa4 black-80">
+                    <label className="db fw4 lh-copy f6" htmlFor="password">Create New User</label>
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                        <legend className="ph0 mh0 fw6 clip">Sign Up</legend>
 
-                    <input
-                        value={ this.state.name } 
-                        type="text" 
-                        className="mb2"
-                        onChange={ (e) => this.setState({
-                            name: e.target.value
-                        }) }
-                        placeholder='Full names of new user'/>
-                </div>
-                <button 
-                    onClick={ () => this._createUser() }>
-                    Save User
-                </button>
+                        <div className="mt3">
+                            <input 
+                                className="pa2 input-reset ba bg-transparent w-100 measure" 
+                                type="text" 
+                                name="salute"  
+                                id="user-salute" 
+                                value={ this.state.salute }
+                                placeholder="Salute ..."
+                                onChange={ (e) => this.setState({
+                                    salute: e.target.value
+                                }) }/>
+                        </div>
+
+                        <div className="mt3">
+                            <input 
+                                className="pa2 input-reset ba bg-transparent w-100 measure" 
+                                type="text" 
+                                name="name"  
+                                id="full-names" 
+                                value={ this.state.name }
+                                placeholder="Full Names ..."
+                                onChange={ (e) => this.setState({
+                                    name: e.target.value
+                                }) }/>
+                        </div>
+
+                        <div className="mt3">
+                            <input 
+                                className="pa2 input-reset ba bg-transparent w-100 measure" 
+                                type="email" 
+                                name="email-address"  
+                                id="email-address" 
+                                value={ this.state.email }
+                                placeholder="Email address ..."
+                                onChange={ (e) => this.setState({
+                                    email: e.target.value
+                                }) }/>
+                        </div>
+
+                        <div className="mt3">
+                            <input 
+                                className="b pa2 input-reset ba bg-transparent w-100 measure" 
+                                type="password" 
+                                name="password"  
+                                id="user-password" 
+                                value={ this.state.password }
+                                placeholder="Password ..."
+                                onChange={ (e) => this.setState({
+                                    password: e.target.value
+                                }) }/>
+                        </div>
+
+                        </fieldset>
+                        <div className="mt3">
+                            <input 
+                                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6" 
+                                type="submit" 
+                                value="Save User" 
+                                onClick={ async () => this._createUser() }/></div>
+                    </article>
             </div>
         )
     }
-    _createUser = async () => {
-        const { salute, name } = this.state
-        await this.props.createUserMutation({
+    _createUser() {
+        var { salute, name } = this.state;
+        this.props.createUserMutation({
             variables: {
                 salute, 
-                name
+                name,
             }
         })
     }
@@ -53,7 +101,7 @@ const CREATE_USER_MUTATION = gql`
     mutation CreateUserMutation(
         $salute:String!,
         $name: String!
-    )
+    ){
     addUser(
         salute: $salute,
         name: $name,
@@ -61,6 +109,7 @@ const CREATE_USER_MUTATION = gql`
         id
         salute
         name
+    }
     }`
 
 export default graphql(CREATE_USER_MUTATION, { name: 'createUserMutation' }) (CraeteUser)
--- views/main.jsx
+++ views/main.jsx
@@ -1,13 +1,13 @@
 'use babel';
 
 import React from 'react';
-import UserList from './userList';
-// import CreateUser from './createUser';
+// import UserList from './userList';
+import CreateUser from './createUser';
 
 export default class Main extends React.Component {
   render() {
     return <div>
-       <UserList />;
+       <CreateUser />;
     </div>
     }
 }
